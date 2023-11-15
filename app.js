88067b7667270c697fc62772ce5700c7383d76fb
jaysmithwebdev <jaysmith.webdevwork@gmail.com>
2023-11-15 12:06:45 +0000

    Update style

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
-const express = require("express");
+import express from "express";
 const port = process.env.PORT || 3001;
+import exhibits from "./public/data/exhibits.js";
+import landing from "./public/data/landing.js";
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  const links = {
+    pageLink: "partials/landing.ejs",
+    contentList: landing,
+    exhibitList: exhibits,
+  };
+  res.render("index.ejs", links);
+});
+
+app.get("/one", (req, res) => {
+  const links = {
+    pageLink: "partials/exhibit.ejs",
+    exhibit: exhibits.exhibit1,
+  };
+  res.render("index.ejs", links);
+});
+
+app.get("/two", (req, res) => {
+  const links = {
+    pageLink: "partials/exhibit.ejs",
+    exhibit: exhibits.exhibit2,
+  };
+  res.render("index.ejs", links);
+});
+
+app.get("/three", (req, res) => {
+  const links = {
+    pageLink: "partials/exhibit.ejs",
+    exhibit: exhibits.exhibit3,
+  };
+  res.render("index.ejs", links);
+});
+
+app.get("/four", (req, res) => {
+  const links = {
+    pageLink: "partials/exhibit.ejs",
+    exhibit: exhibits.exhibit4,
+  };
+  res.render("index.ejs", links);
 });
 
 app.listen(port, () => {
---
+++ public/data/exhibits.js
@@ -0,0 +1,32 @@
+const exhibits = {
+  exhibit1: {
+    name: "Exhibit One",
+    image: "https://picsum.photos/200",
+    audio: "audio/clock.mp3",
+    description:
+      "Exhibit one. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  exhibit2: {
+    name: "Exhibit Two",
+    image: "https://picsum.photos/200",
+    audio: "audio/clock.mp3",
+    description:
+      "Exhibit two. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  exhibit3: {
+    name: "Exhibit Three",
+    image: "https://picsum.photos/200",
+    audio: "audio/clock.mp3",
+    description:
+      "Exhibit three. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  exhibit4: {
+    name: "Exhibit Four",
+    image: "https://picsum.photos/200",
+    audio: "audio/clock.mp3",
+    description:
+      "Exhibit four. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+};
+
+export default exhibits;
