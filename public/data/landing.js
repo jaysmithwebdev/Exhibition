1c209e5a26b1cbdee9d27ed2ed184c1a82a60c46
jaysmithwebdev <jaysmith.webdevwork@gmail.com>
2023-11-17 23:43:30 +0000

    Style text against images

--- public/data/landing.js
+++ public/data/landing.js
@@ -1,6 +1,6 @@
 const landing = {
   name: "Landing Title",
-  image: "https://picsum.photos/200",
+  image: "https://picsum.photos/id/63/200",
   audio: "audio/clock.mp3",
   description:
     "Intro text to the site goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
