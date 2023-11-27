1f1347dab523e1f8e98d825c25b11c0e0c2b3861
jaysmithwebdev <jaysmith.webdevwork@gmail.com>
2023-11-27 20:33:10 +0000

    Add audio to the guide section

--- public/data/landing.js
+++ public/data/landing.js
@@ -4,6 +4,14 @@ const landing = {
   audio: "audio/clock.mp3",
   description:
     "Intro text to the site goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  guide1:
+    "Scan the QR codes next to each of the exhibition pieces to see or hear more about the exhibits.",
+  guide2:
+    "You can hear the information about the exhibition via the embeded audio players found on each page.",
+  guide3:
+    "Individual exhibit pages can also be found using the links at the top of any page on the site.",
+  guide4:
+    "You can view and listen to all exhibits by scrolling through this page.",
 };
 
 export default landing;
