9984755b231b727fc70c38ce2168d3d4af78ee82
jaysmithwebdev <jaysmith.webdevwork@gmail.com>
2023-11-28 00:23:44 +0000

    Add real images, titles, and some content

--- public/data/exhibits.js
+++ public/data/exhibits.js
@@ -1,32 +1,32 @@
 const exhibits = {
   exhibit1: {
-    name: "Exhibit One",
-    image: "https://picsum.photos/id/63/200",
-    audio: "audio/clock.mp3",
+    name: "What A Load",
+    image: "images/WhatALoad1.jpeg",
     description:
       "Exhibit one. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   exhibit2: {
-    name: "Exhibit Two",
-    image: "https://picsum.photos/id/63/200",
-    audio: "audio/clock.mp3",
+    name: "The Mask",
+    image: "images/TheMask1.jpeg",
     description:
       "Exhibit two. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   exhibit3: {
-    name: "Exhibit Three",
-    image: "https://picsum.photos/id/63/200",
-    audio: "audio/clock.mp3",
+    name: "Twisted Into Every Part Of Me",
+    image: "images/Twisted1.jpeg",
     description:
       "Exhibit three. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   exhibit4: {
-    name: "Exhibit Four",
-    image: "https://picsum.photos/id/63/200",
-    audio: "audio/clock.mp3",
+    name: "Death By A Thousand Cuts",
+    image: "images/DeathCuts1.jpeg",
     description:
       "Exhibit four. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
+  audioDescription1: "This is a seperate description.",
+  audioDescription2: "This is a second seperate description.",
+  audioDescription3: "This is the third seperate description.",
+  audioDescription4: "This is the last seperate description.",
 };
 
 export default exhibits;
--- public/data/landing.js
+++ public/data/landing.js
@@ -1,9 +1,10 @@
 const landing = {
-  name: "Landing Title",
-  image: "https://picsum.photos/id/63/200",
-  audio: "audio/clock.mp3",
+  name: "Pulling At The Threads Of Ableism",
+  image: "images/ImFine1.jpeg",
   description:
-    "Intro text to the site goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "This mini exhibition is the culmination of the artist's research and creative exploration into disability identity and how it is affected by an ableist society. Through her lived experience, the artist has explored the feelings and frustrations of living as a disabled person within a world that is not designed to cater to her accessibility needs and in a society that is not comfortable with difference.",
+  artist:
+    "Gillian Ashcroft-Smith is an activist and an artist-facilitator who creates art through mixed bricolage and traditional craft methods. She particularly enjoys creating new visually pleasing pieces from rescued, recovered and reused materials.",
   guide1:
     "Scan the QR codes next to each of the exhibition pieces to see or hear more about the exhibits.",
   guide2:
@@ -12,6 +13,8 @@ const landing = {
     "Individual exhibit pages can also be found using the links at the top of any page on the site.",
   guide4:
     "You can view and listen to all exhibits by scrolling through this page.",
+  toRead:
+    "This mini exhibition is the culmination of the artist's research and creative exploration into disability identity and how it is affected by an ableist society. Through her lived experience, the artist has explored the feelings and frustrations of living as a disabled person within a world that is not designed to cater to her accessibility needs and in a society that is not comfortable with difference. Gillian Ashcroft-Smith is an activist and an artist-facilitator who creates art through mixed bricolage and traditional craft methods. She particularly enjoys creating new visually pleasing pieces from rescued, recovered and reused materials.",
 };
 
 export default landing;
