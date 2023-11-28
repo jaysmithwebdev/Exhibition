eebf130050d871b1e699a6926d6444ad7f8bf68b
jaysmithwebdev <jaysmith.webdevwork@gmail.com>
2023-11-28 20:40:43 +0000

    Fix landing page audio

--- public/data/landing.js
+++ public/data/landing.js
@@ -5,6 +5,8 @@ const landing = {
     "This mini exhibition is the culmination of the artist's research and creative exploration into disability identity and how it is affected by an ableist society. Through her lived experience, the artist has explored the feelings and frustrations of living as a disabled person within a world that is not designed to cater to her accessibility needs and in a society that is not comfortable with difference.",
   artist:
     "Gillian Ashcroft-Smith is an activist and an artist-facilitator who creates art through mixed bricolage and traditional craft methods. She particularly enjoys creating new visually pleasing pieces from rescued, recovered and reused materials.",
+  audio:
+    "This mini exhibition is the culmination of the artists research and creative exploration into disability identity and how it is affected by an ableist society. Through her lived experience, the artist has explored the feelings and frustrations of living as a disabled person within a world that is not designed to cater to her accessibility needs and in a society that is not comfortable with difference. Gillian Ashcroft-Smith is an activist and an artist-facilitator who creates art through mixed bricolage and traditional craft methods. She particularly enjoys creating new visually pleasing pieces from rescued, recovered and reused materials.",
   guide1:
     "Scan the QR codes next to each of the exhibition pieces to see or hear more about the exhibits.",
   guide2:
@@ -13,8 +15,6 @@ const landing = {
     "Individual exhibit pages can also be found using the links at the top of any page on the site.",
   guide4:
     "You can view and listen to all exhibits by scrolling through this page.",
-  toRead:
-    "This mini exhibition is the culmination of the artist's research and creative exploration into disability identity and how it is affected by an ableist society. Through her lived experience, the artist has explored the feelings and frustrations of living as a disabled person within a world that is not designed to cater to her accessibility needs and in a society that is not comfortable with difference. Gillian Ashcroft-Smith is an activist and an artist-facilitator who creates art through mixed bricolage and traditional craft methods. She particularly enjoys creating new visually pleasing pieces from rescued, recovered and reused materials.",
 };
 
 export default landing;
